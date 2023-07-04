c5cb39a47ce32fa50f58bd75ef066505f4912d98
Shon <agito850@gmail.com>
2023-07-05 00:23:40 +0800

    月曆新增活動

--- JavaScript/Day05/Calendar/script.js
+++ JavaScript/Day05/Calendar/script.js
@@ -1,51 +1,295 @@
-
 // 每個月的名稱
-const month_names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 let calendar;
-let month_list;
-let month_picker;
-let curr_month;
-let curr_year;
+let monthList;
+let monthPicker;
+let currMonth;
+let currYear;
 let currDate;
+
+let downLoad;
+
+let schedDate;
+let schedTime;
+let schedLocation;
+let schedContent;
+let schedColor;
+let btnAddSchedule;
+let btnReviseSchedule;
+let btnClearSchedule;
+
+let timeArray;
+let timeYear;
+let timeMonth;
+let timeDate;
+
+let scheduleModal;
+let coverBg;
 
 
 
 window.onload = function () {
+    // localStorage 刪除重置魔法
+    // localStorage.removeItem('EverySchedule');
+
+
+    // 抓 Modal視窗
+    scheduleModal = document.querySelector(".schedule-modal")
+    // 抓 fixed背景
+    coverBg = document.querySelector(".cover-bg")
 
     // 抓 月曆本體
     calendar = document.querySelector(".calendar")
     // 抓 月份總表 
-    month_list = calendar.querySelector(".month-list")
+    monthList = calendar.querySelector(".month-list")
     // 抓 月份按鈕(目前顯示的月份)
-    month_picker = calendar.querySelector("#month-picker")
+    monthPicker = calendar.querySelector("#month-picker")
+
+
     // 製作 月份總表
-    
+
 
     // 先製作一次"現在" 的月曆
     currDate = new Date() // () 中間沒給值的話 就是"現在"的意思
-    curr_month = { value: currDate.getMonth() }
-    curr_year = { value: currDate.getFullYear() }
-    generateCalendar(curr_month.value, curr_year.value)
+    currMonth = { value: currDate.getMonth() }
+    currYear = { value: currDate.getFullYear() }
+    generateCalendar(currMonth.value, currYear.value)
 
     makeMonthList()
 
 
     // 按下月份後 會跳出各個月份的月曆
-    month_picker.addEventListener("click", () => {
-        month_list.classList.add("show")
+    monthPicker.addEventListener("click", () => {
+        monthList.classList.add("show")
     })
 
     // 按下前 後三角 會顯示不同年份
     document.querySelector("#prev-year").addEventListener("click", () => {
-        --curr_year.value
-        generateCalendar(curr_month.value, curr_year.value)
+        --currYear.value
+        generateCalendar(currMonth.value, currYear.value)
     })
     document.querySelector("#next-year").addEventListener("click", () => {
-        ++curr_year.value
-        generateCalendar(curr_month.value, curr_year.value)
+        ++currYear.value
+        generateCalendar(currMonth.value, currYear.value)
     })
 
+    // 按下x Modal消失
+    document.querySelector(".close").addEventListener("click", () => {
+        scheduleModal.style.display = "none"
+        coverBg.style.display = "none"
+    })
+
+    //  ----------印出所有localStorage活動---------------
+    downLoad = JSON.parse(localStorage.getItem('EverySchedule'))
+    printAllSched()
+
+
+
+
+
+    // ----------新增活動---------------
+
+
+    btnAdd = document.querySelector("#add-schedule")
+    btnAddSchedule = document.querySelector("#btn-add-schedule")
+    btnReviseSchedule = document.querySelector("#btn-revise-schedule")
+    btnClearSchedule = document.querySelector("#btn-clear-schedule")
+
+    btnAdd.addEventListener("click", () => {
+        scheduleModal.style.display = "block"
+        coverBg.style.display = "block"
+    })
+
+
+    //檢測 是否 日期 時間 活動內容有寫(地點不一定要)
+    btnAddSchedule.addEventListener("click", () => {
+        saveModalToDownload()
+        creatModalToP()
+    })
+
+
+    //刪除所有活動(重置)
+    btnClearSchedule.addEventListener("click", () => {
+        // localStorage.getItem('EverySchedule')
+        localStorage.removeItem('EverySchedule');
+
+        // 行程以JSON格式設計
+        let sched = {
+            id:"2023-7-15",completeDate: "2023-07-15",year:2023,month:7,date:15,
+            todolist:[{ time:"18:00",at:"學校",todo:"寫作業",color:"#992445" }]
+        }
+        let sched2 = {
+            id:"2023-7-24",completeDate: "2023-07-24",year:2023,month:7,date:24,
+            todolist:[{ time:"09:00",at:"家",todo:"看書",color:"#226445" }]
+        }
+        downLoad=[sched,sched2]
+        // 行程陣列(資料們)轉成字串 儲存在LocalStorage
+        localStorage.setItem("EverySchedule", JSON.stringify(downLoad));
+    })
+
+
+    //按下每個活動p 就跳出該活動的Modal視窗
+    document.querySelectorAll("p").forEach((elementP) => {
+        elementP.addEventListener("click", () => {
+
+            //從downLoad裡尋找與p相同id的物件
+
+            let s = downLoad.find(element => element.id == elementP.id)
+            //開啟Modal
+            scheduleModal.style.display = "block"
+            coverBg.style.display = "block"
+
+            document.querySelector('input[type="date"]').value = s.completeDate
+            document.querySelector('input[type="time"]').value = s.todolist[0].time
+            document.querySelector('.sched-location').value = s.todolist[0].at
+            document.querySelector('.sched-content').value = s.todolist[0].todo
+            document.querySelector('input[type="color"]').value = s.todolist[0].color
+
+
+        })
+
+
+        //修改活動 按鈕
+        btnReviseSchedule.addEventListener("click", () => {
+            //刪除downLoad裡 同id的活動
+            let clearId = downLoad.findIndex(element => element.id == elementP.id)
+            downLoad.splice(clearId, 1)
+
+
+            saveModalToDownload()
+            changeModalToP()
+        })
+
+    })
+
+
+
+
+
+}
+
+
+function printAllSched() {
+    //抓到現有的活動 
+
+    if (downLoad == null) { return }
+    downLoad.forEach(s => {
+        let idArray = s.id.split("-")
+        idYear = parseInt(idArray[0], 10)
+        idMonth = parseInt(idArray[1], 10)
+        idDate = parseInt(idArray[2], 10)
+        // 找出符合的日期格子
+        let theSchedDay = document.querySelector(`.day-${idYear}-${idMonth}-${idDate}`)
+
+        // 建立 p
+        let p = document.createElement("p")
+        p.setAttribute("id", s.id);
+        if (s.todolist[0].at) {
+            p.innerHTML = `<span>● </span> ${s.todolist[0].time} 在${s.todolist[0].at} ${s.todolist[0].todo}`
+        } else {
+            p.innerHTML = `<span>● </span> ${s.todolist[0].time} ${s.todolist[0].todo}`
+        }
+
+
+        //圓點icon換色
+        let pointIcon = p.querySelector("span")
+        pointIcon.style.color = s.todolist[0].color
+
+        theSchedDay.append(p)
+
+
+    })
+
+}
+
+function saveModalToDownload() {
+    schedDate = document.querySelector('input[type="date"]').value
+    schedTime = document.querySelector('input[type="time"]').value
+    schedLocation = document.querySelector('.sched-location').value
+    schedContent = document.querySelector('.sched-content').value
+    schedColor = document.querySelector('input[type="color"]').value
+
+    timeArray = schedDate.split("-")
+    timeYear = parseInt(timeArray[0], 10)
+    timeMonth = parseInt(timeArray[1], 10)
+    timeDate = parseInt(timeArray[2], 10)
+
+    // 行程以JSON格式設計
+    // let sched = {
+    //     id:"2023-07-15",year:2023,month:7,date:28,
+    //     todoolist:[{ time:"18:00",at:"學校",todo:"寫作業",color:"#ff0000" }]
+    // }
+    let sched = {
+        id: `${timeYear}-${timeMonth}-${timeDate}`,
+        completeDate: schedDate,
+        year: timeYear,
+        month: timeMonth,
+        date: timeDate,
+        todolist: [
+            {
+                time: schedTime,
+                at: schedLocation,
+                todo: schedContent,
+                color: schedColor
+            }
+        ]
+    }
+    downLoad.push(sched)
+    // 行程陣列(資料們)轉成字串 儲存在LocalStorage
+    localStorage.setItem("EverySchedule", JSON.stringify(downLoad));
+}
+
+function creatModalToP() {
+    // 將localStorage的行程顯示在月曆上
+    let SchedDayClass = `.day-${timeYear}-${timeMonth}-${timeDate}`
+    let theSchedDay = document.querySelector(SchedDayClass)
+    let p = document.createElement("p")
+    p.setAttribute("id", `${timeYear}-${timeMonth}-${timeDate}`);
+    if (schedLocation) {
+        p.innerHTML = `<span>● </span> ${schedTime} 在${schedLocation} ${schedContent}`
+    } else {
+        p.innerHTML = `<span>● </span> ${schedTime} ${schedContent}`
+    }
+
+
+    //圓點icon換色
+    let pointIcon = p.querySelector("span")
+    pointIcon.style.color = schedColor
+
+
+    //關閉Modal
+    scheduleModal.style.display = "none"
+    coverBg.style.display = "none"
+
+    theSchedDay.append(p)
+}
+
+function changeModalToP() {
+    // 將localStorage的行程顯示在月曆上
+    let SchedDayClass = `.day-${timeYear}-${timeMonth}-${timeDate}`
+    let theSchedDay = document.querySelector(SchedDayClass)
+    //刪除原本的p 建立新p
+    theSchedDay.innerHTML = ""
+    let p = document.createElement("p")
+    p.setAttribute("id", `${timeYear}-${timeMonth}-${timeDate}`);
+    if (schedLocation) {
+        p.innerHTML = `<span>● </span> ${schedTime} 在${schedLocation} ${schedContent}`
+    } else {
+        p.innerHTML = `<span>● </span> ${schedTime} ${schedContent}`
+    }
+
+
+    //圓點icon換色
+    let pointIcon = p.querySelector("span")
+    pointIcon.style.color = schedColor
+
+
+    //關閉Modal
+    scheduleModal.style.display = "none"
+    coverBg.style.display = "none"
+
+    theSchedDay.append(p)
 }
 
 
@@ -60,26 +304,26 @@ function getFebDays(year) {
 }
 
 // 製作 月份總表
-function makeMonthList(){
+function makeMonthList() {
     // 把剛剛列出來的月份一個一個顯示出來 加上html
-    month_names.forEach((m, index) => {
+    monthNames.forEach((m, index) => {
         let monthDiv = document.createElement("div")
         monthDiv.innerHTML = `<div date-month="${index}">${m}</div>`
 
         // 設置每個月的 按鈕事件
         monthDiv.querySelector("div").addEventListener("click", () => {
-            month_list.classList.remove("show")
-            curr_month.value = index
-            generateCalendar(index, curr_year.value)
+            monthList.classList.remove("show")
+            currMonth.value = index
+            generateCalendar(index, currYear.value)
         })
 
-        month_list.append(monthDiv)
+        monthList.append(monthDiv)
     })
 }
 
 // 產生日曆格式
-function generateCalendar(month, year){
-    
+function generateCalendar(month, year) {
+
     // 抓 年 月
     let calendar_days = calendar.querySelector(".calendar-days")
     let calendar_header_year = calendar.querySelector("#year")
@@ -95,8 +339,8 @@ function generateCalendar(month, year){
     // if (!month) { month = currDate.getMonth() }
     // if (!year) { year = currDate.getFullYear() }
 
-    curr_month_name = `${month_names[month]}`
-    month_picker.innerHTML = curr_month_name
+    currMonth_name = `${monthNames[month]}`
+    monthPicker.innerHTML = currMonth_name
     calendar_header_year.innerHTML = year
 
     let first_day = new Date(year, month, 1)
@@ -108,23 +352,22 @@ function generateCalendar(month, year){
     for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
         let dayContainer = document.createElement("div")
         dayContainer.classList.add("day-container")
-        let dayBC;
         let day;
         if (i >= first_day.getDay()) {//確定前面要空幾格
-            dayBC = document.createElement("div")
-            dayBC.classList.add("dayBC")
             day = document.createElement("div")
-            day.classList.add("calendar-day-hover")
+            day.classList.add("dayBC")
             // 目前是第幾格 - 此月份的1日在第幾格 + 從1開始數
-            day.innerHTML = i - first_day.getDay() + 1
-            day.innerHTML += `<span></span>` //裝飾圓圈
-            if (i - first_day.getDay() + 1 === currDate.getDate() && year === currDate.getFullYear()) {
-                day.classList.add("curr-date")
+            day.innerHTML = `<span class="day-num">${i - first_day.getDay() + 1}</span>`
+            day.innerHTML += `<div class="schedToday day-${year}-${month + 1}-${i - first_day.getDay() + 1}"></div>` //當天活動
+            if (i - first_day.getDay() + 1 === currDate.getDate() && month === currDate.getMonth() && year === currDate.getFullYear()) {
+                let todayDiv = day.querySelector(".day-num")
+                todayDiv.classList.add("curr-date")
+
+                // day.classList.add("curr-date")
             }
-            dayBC.append(day)
-            dayContainer.append(dayBC)
+            dayContainer.append(day)
         }
-        
+
         calendar_days.append(dayContainer)
     }
 }
