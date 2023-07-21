92aeb11c4fdea778a32e6cbc93633fb245a5cf4e
Shon <agito850@gmail.com>
2023-07-22 01:39:41 +0800

    新增巧虎

---
+++ JavaScript/Day06/littleTiger/main.js
@@ -0,0 +1,455 @@
+const brickData =[
+    {
+        id: '1',
+        color: 'Pink',
+        icon: 'head_pupu.jpg',
+        iconEnd: 'head_pupu_2.jpg',
+        finalImg: 'center_pupu.jpg',
+        target: function(){
+            return '普普'
+        }
+    },
+    {
+        id: '2',
+        color: 'HotPink',
+        icon: 'head_linlin.jpg',
+        iconEnd: 'head_linlin_2.jpg',
+        finalImg: 'center_linlin.jpg',
+        target: function(){
+            return '玲玲'
+        }
+    },
+    {
+        id: '3',
+        color: 'Orange',
+        icon: 'head_buobuo.jpg',
+        iconEnd: 'head_buobuo_2.jpg',
+        finalImg: 'center_buobuo.jpg',
+        target: function(){
+            return '波波'
+        }
+    },
+    {
+        id: '4',
+        color: 'MediumAquaMarine',
+        icon: 'head_tou.jpg',
+        iconEnd: 'head_tou_2.jpg',
+        finalImg: 'center_tou.jpg',
+        target: function(){
+            return '桃樂比'
+        }
+    },
+    {
+        id: '5',
+        color: 'PaleTurquoise',
+        icon: 'head_dada.jpg',
+        iconEnd: 'head_dada_2.jpg',
+        finalImg: 'center_dada.jpg',
+        target: function(){
+            return '達達'
+        }
+    },
+    {
+        id: '6',
+        color: 'Purple',
+        icon: 'head_meow.jpg',
+        iconEnd: 'head_meow_2.jpg',
+        finalImg: 'center_meow.jpg',
+        target: function(){
+            return '妙妙'
+        }
+    },
+    {
+        id: '7',
+        color: 'FireBrick',
+        icon: 'head_chichi.jpg',
+        iconEnd: 'head_chichi_2.jpg',
+        iconEnd: 'head_chichi_2.jpg',
+        finalImg: 'center_chichi.jpg',
+        target: function(){
+            return '琪琪'
+        }
+    },
+    {
+        id: '8',
+        color: 'Pink',
+        icon: 'head_pupu.jpg',
+        iconEnd: 'head_pupu_2.jpg',
+        finalImg: 'center_pupu.jpg',
+        target: function(){
+            return '普普'
+        }
+    },
+    {
+        id: '9',
+        color: 'HotPink',
+        icon: 'head_linlin.jpg',
+        iconEnd: 'head_linlin_2.jpg',
+        finalImg: 'center_linlin.jpg',
+        target: function(){
+            return '玲玲'
+        }
+    },
+    {
+        id: '10',
+        color: 'Orange',
+        icon: 'head_buobuo.jpg',
+        iconEnd: 'head_buobuo_2.jpg',
+        finalImg: 'center_buobuo.jpg',
+        target: function(){
+            return '波波'
+        }
+    },
+    {
+        id: '11',
+        color: 'MediumAquaMarine',
+        icon: 'head_tou.jpg',
+        iconEnd: 'head_tou_2.jpg',
+        finalImg: 'center_tou.jpg',
+        target: function(){
+            return '桃樂比'
+        }
+    },
+    {
+        id: '12',
+        color: 'PaleTurquoise',
+        icon: 'head_dada.jpg',
+        iconEnd: 'head_dada_2.jpg',
+        finalImg: 'center_dada.jpg',
+        target: function(){
+            return '達達'
+        }
+    },
+    {
+        id: '13',
+        color: 'Purple',
+        icon: 'head_meow.jpg',
+        iconEnd: 'head_meow_2.jpg',
+        finalImg: 'center_meow.jpg',
+        target: function(){
+            return '妙妙'
+        }
+    },
+    {
+        id: '14',
+        color: 'FireBrick',
+        icon: 'head_chichi.jpg',
+        iconEnd: 'head_chichi_2.jpg',
+        iconEnd: 'head_chichi_2.jpg',
+        finalImg: 'center_chichi.jpg',
+        target: function(){
+            return '琪琪'
+        }
+    },
+    {
+        id: '15',
+        color: 'Pink',
+        icon: 'head_pupu.jpg',
+        iconEnd: 'head_pupu_2.jpg',
+        finalImg: 'center_pupu.jpg',
+        target: function(){
+            return '普普'
+        }
+    },
+    {
+        id: '16',
+        color: 'HotPink',
+        icon: 'head_linlin.jpg',
+        iconEnd: 'head_linlin_2.jpg',
+        finalImg: 'center_linlin.jpg',
+        target: function(){
+            return '玲玲'
+        }
+    },
+    {
+        id: '17',
+        color: 'Orange',
+        icon: 'head_buobuo.jpg',
+        iconEnd: 'head_buobuo_2.jpg',
+        finalImg: 'center_buobuo.jpg',
+        target: function(){
+            return '波波'
+        }
+    },
+    {
+        id: '18',
+        color: 'MediumAquaMarine',
+        icon: 'head_tou.jpg',
+        iconEnd: 'head_tou_2.jpg',
+        finalImg: 'center_tou.jpg',
+        target: function(){
+            return '桃樂比'
+        }
+    },
+    {
+        id: '19',
+        color: 'PaleTurquoise',
+        icon: 'head_dada.jpg',
+        iconEnd: 'head_dada_2.jpg',
+        finalImg: 'center_dada.jpg',
+        target: function(){
+            return '達達'
+        }
+    },
+    {
+        id: '20',
+        color: 'Purple',
+        icon: 'head_meow.jpg',
+        iconEnd: 'head_meow_2.jpg',
+        finalImg: 'center_meow.jpg',
+        target: function(){
+            return '妙妙'
+        }
+    },
+    {
+        id: '21',
+        color: 'FireBrick',
+        icon: 'head_chichi.jpg',
+        iconEnd: 'head_chichi_2.jpg',
+        finalImg: 'center_chichi.jpg',
+        target: function(){
+            return '琪琪'
+        }
+    },
+    {
+        id: '22',
+        color: 'Pink',
+        icon: 'head_pupu.jpg',
+        iconEnd: 'head_pupu_2.jpg',
+        finalImg: 'center_pupu.jpg',
+        finalImg: 'center_linlin.jpg',
+        target: function(){
+            return '普普'
+        }
+    },
+    {
+        id: '23',
+        color: 'HotPink',
+        icon: 'head_linlin.jpg',
+        iconEnd: 'head_linlin_2.jpg',
+        finalImg: 'center_linlin.jpg',
+        finalImg: 'center_linlin.jpg',
+        target: function(){
+            return '玲玲'
+        }
+    },
+    {
+        id: '24',
+        color: 'Orange',
+        icon: 'head_buobuo.jpg',
+        iconEnd: 'head_buobuo_2.jpg',
+        finalImg: 'center_buobuo.jpg',
+        target: function(){
+            return '波波'
+        }
+    },
+    {
+        id: '25',
+        color: 'MediumAquaMarine',
+        icon: 'head_tou.jpg',
+        iconEnd: 'head_tou_2.jpg',
+        finalImg: 'center_tou.jpg',
+        target: function(){
+            return '桃樂比'
+        }
+    },
+    {
+        id: '26',
+        color: 'PaleTurquoise',
+        icon: 'head_dada.jpg',
+        iconEnd: 'head_dada_2.jpg',
+        finalImg: 'center_dada.jpg',
+        target: function(){
+            return '達達'
+        }
+    },
+    {
+        id: '27',
+        color: 'Purple',
+        icon: 'head_meow.jpg',
+        iconEnd: 'head_meow_2.jpg',
+        finalImg: 'center_meow.jpg',
+        target: function(){
+            return '妙妙'
+        }
+    },
+    {
+        id: '28',
+        color: 'FireBrick',
+        icon: 'head_chichi.jpg',
+        iconEnd: 'head_chichi_2.jpg',
+        finalImg: 'center_chichi.jpg',
+        target: function(){
+            return '琪琪'
+        }
+    },
+    {
+        id: '29',
+        color: 'Pink',
+        icon: 'head_pupu.jpg',
+        iconEnd: 'head_pupu_2.jpg',
+        finalImg: 'center_pupu.jpg',
+        finalImg: 'center_linlin.jpg',
+        target: function(){
+            return '普普'
+        }
+    },
+    {
+        id: '30',
+        color: 'HotPink',
+        icon: 'head_linlin.jpg',
+        iconEnd: 'head_linlin_2.jpg',
+        finalImg: 'center_linlin.jpg',
+        finalImg: 'center_linlin.jpg',
+        target: function(){
+            return '玲玲'
+        }
+    },
+    {
+        id: '31',
+        color: 'Orange',
+        icon: 'head_buobuo.jpg',
+        iconEnd: 'head_buobuo_2.jpg',
+        finalImg: 'center_buobuo.jpg',
+        target: function(){
+            return '波波'
+        }
+    },
+    {
+        id: '32',
+        color: 'MediumAquaMarine',
+        icon: 'head_tou.jpg',
+        iconEnd: 'head_tou_2.jpg',
+        finalImg: 'center_tou.jpg',
+        target: function(){
+            return '桃樂比'
+        }
+    }
+] 
+
+//宣告全域變數
+let bricks;
+let centerDiv;
+let centerImg;
+
+let steps = 0   //剩餘的步數
+let allSteps = 0    //全部的步數
+let currentIndex = 0    //目前走到哪一格
+let restartNum = 0
+let h3
+let speed;   //速率(越大代表越慢)
+
+window.onload = function(){
+    makeBricks()
+    centerDiv = document.getElementById("center")
+    centerImg = document.createElement("img")
+    centerImg.src = `./img/center_Q.jpg`
+    centerDiv.append(centerImg)
+    h3 = document.querySelector("h3")
+
+    //Start 按鈕事件
+    document.querySelector('button').addEventListener('click',function(){
+        if(restartNum != 0){ restart() }
+        speed = 35
+        //Math.random會取 0 ~ 31 之間的數  取完再+1 給random(1~32之間)
+        let random = Math.floor(Math.random() * brickData.length) + 1
+        console.log(random)
+        steps = random + (2 * bricks.length)//多跑兩圈再走到獎項
+        allSteps = steps
+        turnAround()
+        restartNum++
+    })
+}
+
+// {
+//     id: '32',
+//     color: 'MediumAquaMarine',
+//     icon: 'head_tou.jpg',
+//     iconEnd: 'head_tou_2.jpg',
+//     finalImg: 'center_tou.jpg',
+//     target: function(){
+//         return '桃樂比'
+//     }
+// }
+function makeBricks(){
+    bricks = document.querySelectorAll('[box-id]')
+    bricks = Array.from(bricks).sort((a,b) => {
+        //把抓到的box DOM物件 依照box-id排序
+        return a.getAttributeNode('box-id').value - b.getAttributeNode('box-id').value
+    })
+    console.log(bricks)
+    bricks.forEach((x,index) => {
+        let id = index + 1
+        let data = brickData.find(x => x.id == id)
+        // let icon = document.createElement('i')
+        let img = document.createElement('img')
+        img.src=`./img/${data.icon}`
+        x.append(img)
+        img.style.borderColor = data.color
+        // icon.style.backgroundImage = `url('./img/${data.icon}')`
+        // icon.setAttribute('class',data.icon)
+        // x.append(icon)
+    })
+}
+
+function makeBricksEnd(){
+    bricks.forEach((x,index) => {
+        let id = index + 1
+        let data = brickData.find(x => x.id == id)
+        if(index != currentIndex){
+            let imgEnd = document.createElement('img')
+            imgEnd.src=`./img/${data.iconEnd}`
+            imgEnd.classList.add('img-end')
+            x.append(imgEnd)
+        }
+        
+    })
+}
+
+function restart(){
+    bricks.forEach((x,index) => {
+        if(index != currentIndex){
+            let imgEnd = x.querySelector('.img-end')
+            x.removeChild(imgEnd)
+        }
+    })
+    centerImg.src = `./img/center_Q.jpg`
+    h3.innerHTML =`巧虎的肚子好餓喔!<br>但是忘記帶便當了，怎麼辦呢?`
+    h3.style.fontSize = "22px"
+}
+
+function turnAround(){
+    bricks[currentIndex].classList.remove('active')//先去掉上一圈的紀錄
+    currentIndex++ //0 ~ 31
+
+    if(currentIndex >= bricks.length){  currentIndex = 0 }
+
+    bricks[currentIndex].classList.add('active')
+    steps--
+
+    //一般版本:
+    // if( steps > 0){
+    //     setTimeout(turnAround,speed)
+    // }else{
+    //     //  跑完了
+    //     let msgBox = document.getElementById('msg')
+    //     let val = brickData[currentIndex].target()
+    //     msgBox.innerText = `✎您的動物好朋友  ☛  ${val}`
+    // }
+
+    //變速版本:
+    if( steps > 0){
+        setTimeout(turnAround,speed)
+
+        // 當剩下1/3時減速
+        if(steps < Math.floor((allSteps/3))){   speed += 7 }
+    }else{
+        //  跑完了
+        let val = brickData[currentIndex].target()
+        h3.innerText = `謝謝 ${val} \\|/`
+        h3.style.fontSize = "44px"
+        centerImg.src = `./img/${brickData[currentIndex].finalImg}`
+        makeBricksEnd()
+    }
+
+}
